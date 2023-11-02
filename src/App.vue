6247b91b1e7e3b5ab7470846e648d4f5e8c572c1
unknown <funmilolaadebiyi52@gmail.com>
2023-11-02 20:20:02 +0100

    new changes

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <template>
   <div id="app">
    <header-section/>
-   <hero-section/>
-   <service-section/>
-   <about-section/>
-   <food-category/>
   </div>
 </template>
 
 <script>
  import HeaderSection from "./components/HeaderSection.vue";
- import HeroSection from "./components/HeroSection.vue";
- import ServiceSection from "./components/ServiceSection.vue";
- import AboutSection from "./components/AboutSection.vue";
- import FoodCategory from "./components/FoodCategory.vue" 
+ 
 export default {
   name: 'App',
   components: {
     HeaderSection,
-    HeroSection,
-    ServiceSection,
-    AboutSection,
-    FoodCategory
+
   }
 }
 </script>

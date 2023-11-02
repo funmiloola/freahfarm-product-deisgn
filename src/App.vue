d048eb36698364dba2262c1506592c02d8729c6c
unknown <funmilolaadebiyi52@gmail.com>
2023-11-02 21:09:04 +0100

    nav-updates

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,5 @@ export default {
 </script>
 
 <style scoped>
- #app{
-  width:1440px;
- }
+ 
 </style>
--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,80 +1,93 @@
 <template>
-  <div>
-    <nav>
-        <ul class="nav-section">
-        <li class="freshfarm-logo">FreshFarm</li>
-        <li class="options">Home</li>
-        <li  class="options">About</li>
-        <li  class="options">Contact Us</li>
-        <li  class="options">Testimonials</li>
-        </ul>
-        <ul class="nav-options">
-            <li class="login">Login</li>
-            <li><button>Order Now</button></li>
-        </ul>
-    </nav>
-  </div>
+  <nav>
+    <div class="nav-section">
+      <li class="freshfarm-logo">FreshFarm</li>
+      <!-- <img src="" alt="" /> -->
+    </div>
+    <ul>
+      <li v-for="(nav, index) in navs" :key="index" class="options">
+        {{ nav }}
+      </li>
+    </ul>
+    <div class="nav-btns">
+      <button class="login-btn">Login</button>
+      <button class="order-btn">Order Now</button>
+    </div>
+  </nav>
 </template>
 
 <script>
-
 export default {
-
-}
+  data() {
+    return {
+      navs: ["Home", "About", "Contact Us", "Testimonials"],
+    };
+  },
+};
 </script>
 
 <style scoped>
-  @import url('https://fonts.googleapis.com/css2?family=Commissioner:wght@400;500;700&family=DM+Sans:opsz,wght@9..40,400;9..40,500&family=Inter&family=Josefin+Sans:wght@400;700&family=Karla:wght@400;700&family=Kumbh+Sans:wght@400;700&family=League+Spartan:wght@500;700&family=Lexend+Deca&family=Manrope:wght@800&family=Pacifico&family=Plus+Jakarta+Sans:wght@500;800&family=Roboto:wght@400;700&display=swap');
- *{
-  padding:0;
-  margin:0;
- }
- nav{
-  display:flex;  
-  gap:154px;
- }
- ul{
-  
- }
- li{
-  list-style-type: none;
-  
- }
-.freshfarm-logo{
-  font-family: Pacifico;
-  font-weight:400;
-  font-size:48px;
-  color:#008000;
-  padding-left:121px;
+@import url("https://fonts.googleapis.com/css2?family=Commissioner:wght@400;500;700&family=DM+Sans:opsz,wght@9..40,400;9..40,500&family=Inter&family=Josefin+Sans:wght@400;700&family=Karla:wght@400;700&family=Kumbh+Sans:wght@400;700&family=League+Spartan:wght@500;700&family=Lexend+Deca&family=Manrope:wght@800&family=Pacifico&family=Plus+Jakarta+Sans:wght@500;800&family=Roboto:wght@400;700&display=swap");
+* {
+  padding: 0;
+  margin: 0;
 }
-.nav-section{
+nav {
   display: flex;
-  align-items:center;
+  justify-content: space-between;
+  align-items: center;
+  max-width:1280px;
+  margin:0 auto;
+  padding-top:20px;
+  padding-bottom:20px;
 }
-.options{
-  font-family:inter;
-  font-weight:500;
-  font-size:18px;
-  padding-left:52px;
-} 
-.nav-options{
+ul {
   display:flex;
-  align-items:center;
-  gap:32px;
+  gap:50px;
 }
-.login{
+ ul li {
+  list-style-type: none;
   font-family: inter;
   font-weight: 500;
-  font-size: weight;
+  font-size: 18px;
+  cursor:pointer;
 }
-button{
-  padding:20px 75px 20px 75px;
-  background:#008000;
-  color:#fff;
-  border:none;
-  border-radius:8px;
+.freshfarm-logo {
+  font-family: Pacifico;
+  font-weight: 400;
+  font-size: 48px;
+  color: #008000;
+  list-style-type: none;
+}
+.nav-section {
+  display: flex;
+  align-items: center;
+}
+.options {
+   
+  
+}
+.nav-btns{
+  display: flex;
+  align-items: center;
+  gap: 32px;
+}
+.login-btn {
+  font-family: inter;
+  font-weight: 500;
+  color:#000;
   font-size:18px;
-  font-weight:700;
-  font-family:inter;
+  background: transparent;
+  border:none;
+}
+.order-btn {
+  padding: 20px 45px 20px 45px;
+  background: #008000;
+  color: #fff;
+  border: none;
+  border-radius: 8px;
+  font-size: 18px;
+  font-weight: 700;
+  font-family: inter;
 }
 </style>
